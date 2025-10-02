4a7bd097848d0f4a7d1b18abe597fa121a384a4d
Phuti-SoftElsie <phuti.motimele@softelsie.com>
2025-10-02 11:07:24 +0200

    Top nav responsive

--- src/app/components/shared/core/top-nav/top-nav.component.ts
+++ src/app/components/shared/core/top-nav/top-nav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-nav',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './top-nav.component.html',
   styleUrl: './top-nav.component.css'
 })
-export class TopNavComponent {
+export class TopNavComponent implements OnInit {
+  constructor(private router: Router) { }
 
+  ngOnInit(): void {
+  }
+
+  navigateToDashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
+
+  navigateToPost(): void {
+    this.router.navigate(['/post']);
+  }
+
+  
 }
