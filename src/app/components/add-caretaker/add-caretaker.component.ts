5df134bcbc0376812e06b5f684366cdac37e30eb
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-10-20 10:33:33 +0200

    Error message styled

--- src/app/components/add-caretaker/add-caretaker.component.ts
+++ src/app/components/add-caretaker/add-caretaker.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -8,6 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AddCaretakerComponent {
 
+  addCaretakerForm!: FormGroup;
+  submissionStatus: 'success' | 'error' | null = null;
   permissions = ["Granted", "Denied", "Pending"]
+
+  constructor(private form: FormBuilder) {
+    this.addCaretakerForm = this.form.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      profileImage: ['', [Validators.required]],
+      permission: ['', [Validators.required]]
+    })
+  }
+
+  get name(){
+    return this.addCaretakerForm.get('name')
+  }
+
+  get email(){
+    return this.addCaretakerForm.get('email')
+  }
+
+  get profileImage(){
+    return this.addCaretakerForm.get('profileImage')
+  }
+
+  get permission(){
+    return this.addCaretakerForm.get('permission')
+  }
+
+  onSubmit(){
+    if(this.addCaretakerForm.valid){
+      console.log('Submitted', this.addCaretakerForm.value);
+      this.submissionStatus = 'success';
+    } else {
+      console.log('Form is invalid')
+    }
+  }
 
 }
