8173c92f7d697a939989143108961f929e2e85a4
unknown <murangivh>
2025-10-28 10:15:53 +0200

    More conflicts fixed

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
 import { CommonModule, NgIf } from "@angular/common";
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { UserSettingsComponent } from "../user-settings/user-settings.component";
 import { RouterModule } from "@angular/router";
-import { FormsModule } from '@angular/forms';
 import { ContainerLayoutComponent } from './core/container-layout/container-layout.component';
 import { TopNavComponent } from "./core/top-nav/top-nav.component";
 import { FooterComponent } from "./core/footer/footer.component";
 import { AuthLayoutComponent } from './core/auth-layout/auth-layout.component';
 import { DashFooterComponent } from './core/dash-footer/dash-footer.component';
-=======
-import { CommonModule } from "@angular/common";
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
-import { UserSettingsComponent } from "./core/user-settings/user-settings.component";
-import { RouterModule } from "@angular/router";
-import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
-import { ActionButtonComponent } from './base/action-button/action-button.component';
->>>>>>> origin/Development
-
+import { FormsModule  } from '@angular/forms';
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
    ContainerLayoutComponent,
    TopNavComponent,
    FooterComponent,
@@ -29,17 +18,12 @@ import { ActionButtonComponent } from './base/action-button/action-button.compon
    DashFooterComponent,
    DashFooterComponent
  
-=======
-
-    UserSettingsComponent,
->>>>>>> origin/Development
   ],
 
   imports: [
     FormsModule,
     CommonModule,
     RouterModule,
-<<<<<<< HEAD
     NgIf
   ],
 
@@ -52,16 +36,6 @@ import { ActionButtonComponent } from './base/action-button/action-button.compon
   providers: [],
 
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
-=======
-    
-
-  ],
-  exports: [
-    UserSettingsComponent,
-    RouterModule,
-    CommonModule,
-  ]
->>>>>>> origin/Development
 })
 export class SharedModule { }
 
