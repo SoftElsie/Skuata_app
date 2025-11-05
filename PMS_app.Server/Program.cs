44064b0ea15363d47adae4245b49f4948cedf0a1
unknown <murangivh>
2025-11-05 09:19:52 +0200

    Added few directions on program.cs

--- PMS_app.Server/Program.cs
+++ PMS_app.Server/Program.cs
@@ -19,7 +19,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
 
 app.UseAuthorization();
 
