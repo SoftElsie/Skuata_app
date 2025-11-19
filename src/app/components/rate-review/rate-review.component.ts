68471ca0563f7974e43ab262f72b27b0943c00fd
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-11-19 16:12:58 +0200

    Initial Commit

---
+++ src/app/components/rate-review/rate-review.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rate-review',
+  templateUrl: './rate-review.component.html',
+  styleUrl: './rate-review.component.css'
+})
+export class RateReviewComponent {
+
+}
