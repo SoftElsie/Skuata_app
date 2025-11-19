ba858d17aedf83f4545132235a73fa303c6bf41a
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-11-19 18:10:48 +0200

    Form design: done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AddCaretakerComponent } from './components/add-caretaker/add-caretaker.component';
+import { RateReviewComponent } from './components/rate-review/rate-review.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,7 @@ const routes: Routes = [
   },
   {   path:'dash',component:DashboardComponent},
   {   path:'add-caretaker',component:AddCaretakerComponent},
+  {   path:'rate-review',component:RateReviewComponent},
 
 ];
 
