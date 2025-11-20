26583ff5cf4b3d9ab952528197c32860897cbea5
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-11-20 10:16:39 +0200

    Interactivity added...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AddCaretakerComponent } from './components/add-caretaker/add-caretaker.component';
 import { RateReviewComponent } from './components/rate-review/rate-review.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 const routes: Routes = [
@@ -18,7 +20,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), CommonModule],
+  exports: [RouterModule, FormsModule, ReactiveFormsModule]
 })
 export class AppRoutingModule { }
