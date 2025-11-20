11de8ad9dfadb3ab9c280242b20b25a28fc8fba6
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-11-20 10:17:07 +0200

    Start animation added

--- src/app/components/rate-review/rate-review.component.ts
+++ src/app/components/rate-review/rate-review.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-rate-review',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './rate-review.component.css'
 })
 export class RateReviewComponent {
+
+  rateReviewForm!: FormGroup;
+  submissionStatus: 'success' | 'error' | null = null;
+
+  rating = 0;
+  setRating(value: number) {
+    this.rating = value;
+  }
+
+  constructor(private form: FormBuilder){
+    this.rateReviewForm = this.form.group({
+      review: ['']
+    })
+  }
+
+  get review(){
+    return this.rateReviewForm.get('review')
+  }
+
+  onSubmit(){
+    console.log(this.rating)
+  }
 
 }
